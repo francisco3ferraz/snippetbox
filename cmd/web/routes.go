d69b5fe4e877026e0ff163a378b9982fce122b92
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-07-20 11:02:53 +0100

    Not found method to controller

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,10 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
@@ -19,6 +23,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
 	return standard.Then(router)
+
 }
